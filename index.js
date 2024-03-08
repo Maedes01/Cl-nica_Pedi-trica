37c23945aaa98a4eeffa7f4dafc84eb4309b1029
Maedes01 <maedespf@gmail.com>
2024-03-07 22:27:41 -0300

    A2

---
+++ index.js
@@ -0,0 +1,6 @@
+// Função para alternar a visibilidade da lista do menu
+function toggleMenu() {
+    var menuList = document.getElementById("menuList");
+    menuList.classList.toggle("active"); // Adiciona ou remove a classe "active" na lista do menu
+  }
+  
